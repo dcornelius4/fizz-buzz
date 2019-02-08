72b3fe10fa63f85155408096a65a5b9a00c16eca
Dave Cornelius <d_cornelius94@yahoo.com>
2019-02-08 12:51:07 -0800

    almost finished fizzbuzz

---
+++ tests/tests.js
@@ -0,0 +1,38 @@
+const test = QUnit.test;
+
+function fizzBuzz(variable) {
+    if(variable % 15 === 0) {
+        return 'fizzbuzz';
+    } else if(variable % 3 === 0) {
+        return 'fizz'; 
+    } 
+    else if(variable % 5 === 0) {
+        return 'buzz'; 
+    } else {
+        return variable; 
+    }  
+}
+
+
+test('return a number divisble by 3', function(assert) {
+    let result = fizzBuzz(6);
+    assert.equal(result, 'fizz');
+});
+
+test('return a number divisble by 5', function(assert) {
+    let result = fizzBuzz(10);
+    assert.equal(result, 'buzz');
+});
+
+test('numbers divisible by both 3 and 5', function(assert) {
+    let result = fizzBuzz(15);
+    assert.equal(result, 'fizzbuzz');
+});
+
+test('all other numbers', function(assert){
+    let result = fizzBuzz(8);
+    assert.equal(result);
+}); 
+
+
+
